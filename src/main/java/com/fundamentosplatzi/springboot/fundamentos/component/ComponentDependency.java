c7d48390f263307d5663d49cc49cc0c9ddb01d01
orlychaparro <orlychaparro@gmail.com>
2023-05-19 16:49:11 -0400

    primer component Hola Mundo

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentDependency.java
@@ -0,0 +1,7 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.boot.CommandLineRunner;
+
+public interface ComponentDependency  {
+    void saludar();
+}
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentImplement.java
@@ -0,0 +1,14 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentImplement implements ComponentDependency {
+
+    @Override
+    public void saludar() {
+        System.out.println("Hola munndo");
+
+    }
+}
